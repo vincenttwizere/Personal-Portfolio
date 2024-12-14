c1c5a8ac5ef562132daf73d4498e1ec73c6f1c42
twizerevincent <vincenttwizere12@gmail.com>
2024-12-14 17:55:03 +0300

    updates

---
+++ index.js
@@ -0,0 +1,5 @@
+// Function to toggle the visibility of the navbar links on small screens
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links'); // Select the navbar links
+    navLinks.classList.toggle('active'); // Toggle the 'active' class to show or hide the links
+}
