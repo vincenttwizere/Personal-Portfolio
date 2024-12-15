fc1cd161ad98eba3a897f9fb1c6dd9a714ab0f29
vincenttwizere <v.twizere@alustudent.com>
2024-12-15 09:54:46 +0300

    adding js typing function

--- index.js
+++ index.js
@@ -3,3 +3,27 @@ function toggleMenu() {
     const navLinks = document.querySelector('.nav-links'); // Select the navbar links
     navLinks.classList.toggle('active'); // Toggle the 'active' class to show or hide the links
 }
+// Function to simulate typing effect
+var i = 0;
+var j = 0;
+var texts = ['I\'m Vincent Twizere.', 'Web Developer,', 'and UX/UI Designer.'];
+var speed = 50;
+function typeWriter() {
+    if (i < texts[j].length) {
+        document.getElementById("demo").innerHTML += texts[j].charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    } else {
+        // Move to the next text after a delay
+        setTimeout(() => {
+            i = 0;
+            document.getElementById("demo").innerHTML = '';
+            j = (j + 1) % texts.length;
+            typeWriter();
+        }, 1000); 
+    }
+}
+// Start the typing animation when the page loads
+document.addEventListener('DOMContentLoaded', (event) => {
+    typeWriter();
+});
